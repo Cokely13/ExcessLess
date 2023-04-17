7d2f9d059c927fdf08828c4354917a1fbffe5fa4
Ryan Cokely <ryan.cokely@gmail.com>
2023-04-16 21:04:15 -0400

    added models

---
+++ server/db/models/Treat.js
@@ -0,0 +1,28 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+
+const Treat = db.define('treat', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        unique: true,
+    },
+   name: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      unique: false
+  },
+    cals: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    size: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+  },
+})
+
+
+module.exports = Treat
