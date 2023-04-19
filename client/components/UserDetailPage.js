d054f37202d532a26b206bea709f10f5986e6f84
Ryan Cokely <ryan.cokely@gmail.com>
2023-04-19 10:38:11 -0400

    adding totals

--- client/components/UserDetailPage.js
+++ client/components/UserDetailPage.js
@@ -8,28 +8,21 @@ function UserDetailPage() {
   const { userId } = useParams();
   const user = useSelector((state) => state.singleUser);
   const [showEntries, setShowEntries] = useState(false);
-  const [uniqueDates, setUniqueDates] = useState([]);
+  const [showWeek, setShowWeek] = useState(false);
 
   useEffect(() => {
     dispatch(fetchSingleUser(userId));
   }, [dispatch, userId]);
 
   const entries = user.entries
-  // const uniqueIds = [...new Set(entries.map(item => item.date))];
-
-  // const unique = user ? user.entries ? Array.from(new Set(user.entries.map(entry => entry.date))) : 0 : 0
-
-  console.log('et', entries)
-
-  console.log("check", typeof entries)
-
-  // const unique = entries ? Array.from(new Set(entries.map(entry => entry.date))) : 0
-
-
-
 
   const handleShowEntries = () => {
     setShowEntries(!showEntries);
+    setShowWeek("")
+  };
+
+  const handleShowWeek = (date) => {
+    setShowWeek(date)
   };
 
   return (
@@ -46,7 +39,7 @@ function UserDetailPage() {
               ) : (
                 <div></div>
               )}
-              <button onClick={handleShowEntries}>Show Entries</button>
+              {!showEntries ? <button onClick={handleShowEntries}>Show Entries</button> : <button onClick={handleShowEntries}>Hide Entries</button>}
             </>
           ) : (
             <div className="loading-message">Loading...</div>
@@ -54,13 +47,15 @@ function UserDetailPage() {
         </div>
       </div>
       {showEntries && (
-        <div className="entries-container">
           <div className="entry-date-list">
             <h3>Entries by Date</h3>
             { (Array.from(new Set(user.entries.map((entry => entry.date))))).map((date) => (
-              <button key={date} onClick={() => console.log(date)}>{date}</button>
+              <button key={date} onClick={() => handleShowWeek(date)}>{date}</button>
             ))}
           </div>
+                )}
+          {showWeek && (
+          <div className="entries-container">
           <table className="entries-table">
             <thead>
               <tr>
@@ -68,23 +63,160 @@ function UserDetailPage() {
                 <th>Treat</th>
                 <th>Quantity</th>
                 <th>Cals</th>
+                <th>DailyLbs</th>
+                <th>WeeklyLbs</th>
               </tr>
             </thead>
             <tbody>
-              {user.entries.map((entry) => (
+              {user.entries.filter(entry => entry.date == showWeek).map((entry) => (
                 <tr key={entry.id}>
-                  <td>{new Date(entry.date).toISOString().substring(0, 10)}</td>
+                  <td>{entry.date}</td>
                   <td>{entry.treatName}</td>
                   <td>{entry.number}</td>
                   <td>{entry.cals}</td>
+                  <td>{entry.number}</td>
+                  <td>{(((entry.cals * entry.number)/3500)/7).toFixed(2)}</td>
+                  <td>{((entry.cals * entry.number)/3500).toFixed(2)}</td>
                 </tr>
               ))}
+              <tr>      <td>Totals</td>
+                  <td></td>
+                  <td>{user.entries.filter(entry => entry.date == showWeek).reduce(
+    (acc, curr) => acc + parseFloat(curr.number),
+   0)}</td>
+                  <td> {user.entries.filter(entry => entry.date == showWeek).reduce(
+    (acc, curr) => acc + parseFloat(curr.cals),
+   0)}</td>
+                  <td>0</td>
+                  <td>0</td>
+                  <td>0</td></tr>
             </tbody>
           </table>
-        </div>
-      )}
+        </div>)}
     </div>
   );
+// function UserDetailPage() {
+//   const dispatch = useDispatch();
+//   const { userId } = useParams();
+//   const user = useSelector((state) => state.singleUser);
+//   const [showEntries, setShowEntries] = useState(false);
+//   const [showWeek, setShowWeek] = useState(false);
+
+//   useEffect(() => {
+//     dispatch(fetchSingleUser(userId));
+//   }, [dispatch, userId]);
+
+//   const entries = user.entries;
+
+//   const handleShowEntries = () => {
+//     setShowEntries(!showEntries);
+//     setShowWeek("");
+//   };
+
+//   const handleShowWeek = (date) => {
+//     setShowWeek(date);
+//     console.log("date", date);
+//   };
+
+//   // calculate the total of Quantity and Cals columns
+//   const totalQuantity = entries.reduce(
+//     (acc, curr) => acc + parseFloat(curr.number),
+//     0
+//   );
+//   const totalCals = entries.reduce(
+//     (acc, curr) => acc + parseFloat(curr.cals),
+//     0
+//   );
+
+//   // calculate the WeeklyLbs value for the selected week
+//   const weeklyLbs =
+//     (entries
+//       .filter((entry) => entry.date === showWeek)
+//       .reduce(
+//         (acc, curr) => acc + parseFloat(curr.number) * parseFloat(curr.cals),
+//         0
+//       ) /
+//       3500)
+//       .toFixed(2) || 0;
+
+//       return (
+//         <div className="user-detail-page">
+//           <div className="user-header">
+//             <div className="user-info">
+//               {user ? (
+//                 <>
+//                   <h1 className="user-name">{user.username}</h1>
+//                   {user.entries ? (
+//                     <p className="user-entries">
+//                       Number of entries: {user.entries.length}
+//                     </p>
+//                   ) : (
+//                     <div></div>
+//                   )}
+//                   {!showEntries ? (
+//                     <button onClick={handleShowEntries}>Show Entries</button>
+//                   ) : (
+//                     <button onClick={handleShowEntries}>Hide Entries</button>
+//                   )}
+//                 </>
+//               ) : (
+//                 <div className="loading-message">Loading...</div>
+//               )}
+//             </div>
+//           </div>
+//           {showEntries && (
+//             <div className="entries-container">
+//               <div className="entry-date-list">
+//                 <h3>Entries by Date</h3>
+//                 {Array.from(new Set(entries.map((entry) => entry.date))).map(
+//                   (date) => (
+//                     <button
+//                       key={date}
+//                       onClick={() => handleShowWeek(date)}
+//                     >{date}</button>
+//                   )
+//                 )}
+//               </div>
+//               <table className="entries-table">
+//                 <thead>
+//                   <tr>
+//                     <th>Date</th>
+//                     <th>Treat</th>
+//                     <th>Quantity</th>
+//                     <th>Cals</th>
+//                     <th>Totals</th>
+//                     <th>WeeklyLbs</th>
+//                   </tr>
+//                 </thead>
+//                 <tbody>
+//                   {entries
+//                     .filter((entry) => (showWeek ? entry.date === showWeek : true))
+//                     .map((entry) => (
+//                       <tr key={entry.id}>
+//                         <td>{entry.date}</td>
+//                         <td>{entry.treatName}</td>
+//                         <td>{entry.number}</td>
+//                         <td>{entry.cals}</td>
+//                         <td></td>
+//                         <td>
+//                           {(parseFloat(entry.number) * parseFloat(entry.cals) / 3500).toFixed(2)}
+//                         </td>
+//                       </tr>
+//                     ))}
+//                   <tr>
+//                     <td colSpan="2">Totals</td>
+//                     <td>{totalQuantity}</td>
+//                     <td>{totalCals}</td>
+//                     <td></td>
+//                     <td>{(weeklyLbs > 0 ? weeklyLbs : 0)}</td>
+//                   </tr>
+//                 </tbody>
+//               </table>
+//             </div>
+//           )}
+//         </div>
+//       );
+
 }
 
 export default UserDetailPage;
